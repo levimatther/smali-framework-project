ef879ce982b5f7c35ff5f50b9c58a362b1498501
elisam98 <cornbeefonrye@hotmail.com>
2023-02-12 11:08:38 -0500

    rename apk to reflect tenant name

--- change_tenant.sh
+++ change_tenant.sh
@@ -1,5 +1,5 @@
 grep -rl "dil3mm4tenant" --exclude=\*.sh | xargs sed -i "s/dil3mm4tenant/$1/g"
 apktool b .
-zipalign 4 dist/GcsDialer.apk dist/DIA_TCL.apk
-apksigner sign --key keys/shared.pk8 --cert keys/shared.x509.pem --in ./dist/DIA_TCL.apk
+zipalign 4 dist/GcsDialer.apk dist/DIA_TCL-$1.apk
+apksigner sign --key keys/shared.pk8 --cert keys/shared.x509.pem --in ./dist/DIA_TCL-$1.apk
 git reset --hard
