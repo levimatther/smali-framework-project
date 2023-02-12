c46005e96426c3a6d992b5d16c00da7857b18a69
Francesco "Dil3mm4" Manzo <dil3mm4.dev@gmail.com>
2023-02-11 19:13:09 -0500

    Add change tenant script

---
+++ change_tenant.sh
@@ -0,0 +1,5 @@
+grep -rl "dil3mm4tenant" --exclude=\*.sh | xargs sed -i "s/dil3mm4tenant/$1/g"
+apktool b .
+mv ./dist/GcsDialer.apk ./dist/DIA_TCL.apk
+apksigner sign --key keys/shared.pk8 --cert keys/shared.x509.pem --in ./dist/DIA_TCL.apk
+git reset --hard
